7875b9cec879fa12a7c93859c59f4aaee0e12484
nitinsharma1845 <nitinsharma992740@gmail.com>
2025-07-11 23:48:13 +0530

    utils code

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import cookieParser from "cookie-parser";
+import cors from 'cors'
+import express from "express";
+
+const app = express();
+
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+})) // app.use use for configure middlewares
+
+app.use(express.json({limit : "16kb"}))
+app.use(express.urlencoded({extended : true , limit : "16kb"}))
+app.use(express.static('public'))
+app.use(cookieParser())
+
+
+
+export {app} 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import dotenv from "dotenv";
-dotenv.config({path : './.env'});
+dotenv.config({ path: "./.env" });
 import connectDb from "./db/index.js";
-
+import { app } from "./app.js";
 
 connectDb()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error ::: ", error);
+      throw error
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server stared at http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Connection Failed to DataBase");
+  });
