2cebb1f64ca0e31df781c020388f8ea4fdbd0b0d
nitinsharma1845 <nitinsharma992740@gmail.com>
2025-07-14 21:27:10 +0530

    Configuration for writting api and testing with postman

--- src/app.js
+++ src/app.js
@@ -13,6 +13,11 @@ app.use(express.urlencoded({extended : true , limit : "16kb"}))
 app.use(express.static('public'))
 app.use(cookieParser())
 
+//routes
 
+import userRouter from "./routes/user.routes.js";
+
+//routes decelartion
+app.use("/api/v1/users" , userRouter)
 
 export {app} 
